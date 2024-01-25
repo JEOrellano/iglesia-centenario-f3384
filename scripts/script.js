3d4b671075f8677b2d81e4352c37e1231effc97a
Joselo Esteban Orellano <mobilelegendsbang2020bang@gmail.com>
2024-01-24 22:44:14 -0300

    quinto commit genrenando arhivos htl de las paginas

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+/**
+ * Footer
+ */
+// Fecha actual
+const footerFecha = document.querySelector("footer span");
+console.log(footerFecha);
+footerFecha.textContent = "© " + new Date().getFullYear() + " IEADCR."
